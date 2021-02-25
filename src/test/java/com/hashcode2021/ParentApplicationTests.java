a6d693107b8107a53e1abcbf58eb5a260de1544f
lorenzri <rilo1992@gmail.com>
2021-02-25 16:23:23 +0100

    initial commit

---
+++ src/test/java/com/hashcode2021/ParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hashcode2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
